c9011823d889e4b7161303a98ec9c4a840c1cdcd
Byxs20 <97766819@qq.com>
2022-10-10 15:24:33 +0800

    first commit

---
+++ trid.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', type=str, required=True,
+                    help='输入文件路径')
+args  = parser.parse_args()
+
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+file_dir = os.path.abspath(args.f)
+file_name = file_dir.split("\\")[-1]
+
+# 1.复制文件到trid文件夹下
+shutil.copy(file_dir, base_dir)
+
+# 2.开始
+os.chdir(base_dir)
+os.system(f"trid.exe {file_name}")
+os.remove(file_name)
